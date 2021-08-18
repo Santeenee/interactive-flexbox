f5732d5314244812d274cc8b514bbe8f6e9679cd
Santeenee <francescotn02@gmail.com>
2021-08-18 18:58:45 +0200

    added flex-grow box, added sass partials

--- public/app.js
+++ public/app.js
@@ -1,34 +1,58 @@
 const buttons = document.querySelectorAll("button");
 const flexBox = document.querySelectorAll(".flex-box");
 
-const changeFlexboxProperty = (btnId, count, flexProperty) => {
-  flexBox[count].style[flexProperty] = btnId;
+//flexGrow stuff
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll('input[type="number"]');
+const innerBoxes = document.querySelectorAll(".inner-box");
+
+const changeFlexboxProperty = (btnId, count, flexProperty, whatDiv) => {
+  if (whatDiv === "parent") {
+    flexBox[count].style[flexProperty] = btnId;
+    //
+  } else if (whatDiv === "child") {
+    //
+    for (let i = 0; i < inputs.length; i++) {
+      //
+      if (inputs[i].value > 99 || inputs[i].value < 1) {
+        console.log(`invalid input[${i}] --> ${inputs[i].value}`);
+        inputs[i].value = "";
+      }
+      innerBoxes[i].style.flexGrow = inputs[i].value;
+    }
+  } else {
+    console.log("we have a problem here...");
+  }
 };
 
 const doSomething = (btnId, btnClass) => {
   switch (btnClass) {
     case "flex-direction":
-      changeFlexboxProperty(btnId, 0, "flexFlow");
+      changeFlexboxProperty(btnId, 0, "flexFlow", "parent");
       break;
 
     case "justify-content":
-      changeFlexboxProperty(btnId, 1, "justifyContent");
+      changeFlexboxProperty(btnId, 1, "justifyContent", "parent");
       break;
 
     case "align-items":
-      changeFlexboxProperty(btnId, 2, "alignItems");
+      changeFlexboxProperty(btnId, 2, "alignItems", "parent");
       break;
 
     case "combine-justify":
-      changeFlexboxProperty(btnId, 3, "justifyContent");
+      changeFlexboxProperty(btnId, 3, "justifyContent", "parent");
       break;
 
     case "combine-align":
-      changeFlexboxProperty(btnId, 3, "alignItems");
+      changeFlexboxProperty(btnId, 3, "alignItems", "parent");
+      break;
+
+    case "input-number":
+      changeFlexboxProperty(btnId, 4, "flexGrow", "child");
       break;
 
     default:
-      alert("this button doesn't have a class...");
+      //console.log("this button doesn't have the required class...");
       break;
   }
 };
@@ -36,5 +60,11 @@ const doSomething = (btnId, btnClass) => {
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
     doSomething(btn.id, btn.className);
+  });
+});
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    doSomething(input.id, input.className);
   });
 });
